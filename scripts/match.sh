9a94f453f480e104ece8dd1575417ac7e4514349
omid0390 <omid.mesgarha@gmail.com>
2022-08-02 00:12:03 +0430

    remove extra comment
    Former-commit-id: 2fcde7a1ba9346b98f68bd1d92a1be517c442709

--- scripts/match.sh
+++ scripts/match.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #curl "https://raw.githubusercontent.com/SharifAIChallenge/final-judgment/master/resources/map.config" > /tmp/match/map.config
-# curl "https://raw.githubusercontent.com/SharifAIChallenge/AIC21-Minigame/main/map.config" > /tmp/match/map.config
-# curl "https://raw.githubusercontent.com/SharifAIChallenge/AIC21-Game/main/mini_server/map.config" > /tmp/match/map.config
 (cd /tmp/match && java -jar /usr/local/match/match.jar $@)
