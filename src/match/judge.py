116e70cf3da8bbc9be5a3d076fc6b34d1a6616b5
MohammadHosein Bahmani <mohammad.bahmani@tapsi.cab>
2022-08-06 21:24:36 +0430

    Fix stats
    Former-commit-id: b8410ab4edf5bce5221293d06fdd3bcf6fc63250

--- src/match/judge.py
+++ src/match/judge.py
@@ -146,7 +146,7 @@ def judge(players, map_id, game_id) -> [Event]:
     exit_code=__judge()
     try:
         # extract the match stats      
-        stats = str(json.load(open(match_record_path))[STATS_KEYNAME])
+        stats = "{\"stats\": {}}".format(str(json.load(open(match_record_path))[STATS_KEYNAME]))
     except:
         stats = """{"stats": {"winner": -1}}"""
         logger.warning("failed fo fetch match stats")
