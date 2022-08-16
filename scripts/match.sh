bf0cb88ea4c073ef66913394cb792a46e8478cab
mhbahmani <mhbahmani79@gmail.com>
2022-08-16 23:27:04 +0430

    Add client sleep
    Former-commit-id: 6d7b5b91ddde493dc7e86756fe48a7faa67b6808

--- scripts/match.sh
+++ scripts/match.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #curl "https://raw.githubusercontent.com/SharifAIChallenge/final-judgment/master/resources/map.config" > /tmp/match/map.config
-(cd /tmp/match && java -jar /usr/local/match/match.jar $@)
+(cd /tmp/match && clientReadinessThresholdTimeMillisecond=60000 java -jar /usr/local/match/match.jar $@)
