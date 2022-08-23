dc157b49c852b3d00cf821a0fd00b689f5e546dc
MohammadHosein Bahmani <mohammad.bahmani@tapsi.cab>
2022-08-23 21:30:05 +0430

    Set movementLogThreshold
    Former-commit-id: 6194e7087a6f1410748a9585f8661258f39d7230

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ update
 update to 0.1.2  
 update to 0.1.4  
 update to 0.1.6 (fix turn problem)  
+update to 0.1.6.2 (fix turn problem)  
--- scripts/match.sh
+++ scripts/match.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #curl "https://raw.githubusercontent.com/SharifAIChallenge/final-judgment/master/resources/map.config" > /tmp/match/map.config
-(cd /tmp/match && clientReadinessThresholdTimeMillisecond=60000 java -jar /usr/local/match/match.jar $@)
+(cd /tmp/match && movementLogThresholdTimeMillisecond=1500 clientReadinessThresholdTimeMillisecond=60000 java -jar /usr/local/match/match.jar $@)
