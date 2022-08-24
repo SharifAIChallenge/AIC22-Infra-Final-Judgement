6dbf23df919662f036897fe18dc5e89533ca8201
MohammadHosein Bahmani <mhbahmani79@gmail.com>
2022-08-24 19:16:39 +0000

    Update match.sh
    Former-commit-id: d197d106c09cbbe9de2e24dde63977a7897dc8ef

--- scripts/match.sh
+++ scripts/match.sh
@@ -2,5 +2,5 @@
 #curl "https://raw.githubusercontent.com/SharifAIChallenge/final-judgment/master/resources/map.config" > /tmp/match/map.config
 export movementLogThresholdTimeMillisecond=1500
 export clientReadinessThresholdTimeMillisecond=60000
-export magicTurnTime=1500
-(cd /tmp/match && movementLogThresholdTimeMillisecond=1500 clientReadinessThresholdTimeMillisecond=60000 magicTurnTime=1500 java -jar /usr/local/match/match.jar $@)
+export magicTurnTime=2000
+(cd /tmp/match && movementLogThresholdTimeMillisecond=1500 clientReadinessThresholdTimeMillisecond=60000 magicTurnTime=2000 java -jar /usr/local/match/match.jar $@)
