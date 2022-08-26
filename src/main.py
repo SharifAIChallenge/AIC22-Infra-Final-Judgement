47292e0b085442d4f78cf95acd8d452abfff5ea9
MohammadHosein Bahmani <mohammad.bahmani@tapsi.cab>
2022-08-26 16:14:14 +0430

    Add check game
    Former-commit-id: c928dcea9b697dffaea559056d6af4954d14d3f5

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import events
 import logging
 import log
 import traceback
+import requests
 from match import match_queue as mq
 
 print("is ready", flush=True)
@@ -17,6 +18,15 @@ while True:
             continue
         
         token=match.game_id
+        # Check game is real!
+        data = {
+            "token": token
+        }
+        result = requests.get("https://api.aichallenge.ir/api/infra-gateway/game", params=data, headers={'Content-Type': 'application/json; charset=utf-8'})
+        if result.status_code == 404:
+            print("///////////////////////// Skipping ////////////////////////////")
+            continue
+    
         print(f"game_id: {token}", flush=True)
         log.new_token_logger(token)
         events.push(events.Event(token=token, status_code=events.EventStatus.MATCH_STARTED.value,title='match started successfully!'))
