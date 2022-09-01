3b69a23facba9164b8113bbb1f9e7e639ea7e7f7
mohammadreza eskandari <mohammad.1392.sk@gmail.com>
2022-09-01 08:34:49 +0000

    Update match.py
    Former-commit-id: 2f812986533fc5a285a5a0bc758e42f4945073d5

--- src/match/match.py
+++ src/match/match.py
@@ -7,5 +7,5 @@ class Match:
         self.map_id=map_id
 
     def hold(self, first_team_name: str, second_team_name: str)->[Event]:
-        return judge(players=self.players,map_id=self.map_id,game_id=self.game_id, first_team_name, second_team_name)
+        return judge(players=self.players,map_id=self.map_id,game_id=self.game_id, first_team_name=first_team_name, second_team_name=second_team_name)
         
