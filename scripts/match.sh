badfaf722e9b5e984fc3179fbd74d14f22783948
mhbahmani <mhbahmani79@gmail.com>
2022-09-01 17:29:38 +0430

    Increase movement treshold
    Former-commit-id: 046ff2b87d9a20bf3b83356549a265ade8a1184f

--- scripts/match.sh
+++ scripts/match.sh
@@ -4,4 +4,4 @@ export movementLogThresholdTimeMillisecond=1500
 export clientReadinessThresholdTimeMillisecond=60000
 export magicTurnTime=2000
 
-(cd /tmp/match && movementLogThresholdTimeMillisecond=1500 clientReadinessThresholdTimeMillisecond=60000 magicTurnTime=2000 java -jar /usr/local/match/match.jar $@)
+(cd /tmp/match && movementLogThresholdTimeMillisecond=2500 clientReadinessThresholdTimeMillisecond=60000 magicTurnTime=2000 java -jar /usr/local/match/match.jar $@)
